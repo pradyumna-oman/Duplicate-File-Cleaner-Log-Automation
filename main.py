e118c6f67127b7bf24d32c601f9a4634c9361b64
pradyumna-tec <pradyumnao.tech@gmail.com>
2026-07-16 21:24:32 +0530

    Initial project structure and setup

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Duplicate File Cleaner & Log Automation
+
+Version : 0.1.0
+
+Author : Pradyumna Oman
+
+"""
+
+def main():
+
+    print("=" * 50)
+    print("Duplicate File Cleaner & Log Automation")
+    print("=" * 50)
+    print("Application Started Successfully")
+    print("=" * 50)
+
+
+if __name__ == "__main__":
+    main()
